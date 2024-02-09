977e3f7f18451fb969d3b1e40d4ee86d2accfd95
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-02-09 22:58:35 +0530

    action buttons are adde

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,13 @@
-import { Content, DraggableTopBar, RootLayout, Sidebar } from './components'
+import { ActionButtonsRow, Content, DraggableTopBar, RootLayout, Sidebar } from './components'
 
 function App(): JSX.Element {
   return (
     <>
       <DraggableTopBar />
       <RootLayout>
-        <Sidebar className="p-2"></Sidebar>
+        <Sidebar className="p-2">
+          <ActionButtonsRow className="flex justify-between mt-1" />
+        </Sidebar>
         <Content className="border-l bg-zinc-900/50 border-l-white/20"></Content>
       </RootLayout>
     </>
--- src/renderer/src/components/index.ts
+++ src/renderer/src/components/index.ts
@@ -1,3 +1,4 @@
+export * from './ActionButtonsRow'
 export * from './AppLayout'
 export * from './Button'
 export * from './DraggableTopbar'
